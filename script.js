9e66d8ac3b4a08c8928f4ac9b87efd5bc8706993
dush-ik <dushik@live.com>
2022-11-01 21:25:13 +0530

    flipped card fixed

--- script.js
+++ script.js
@@ -1,61 +1,79 @@
 (() => {
-  const divList = Array.from(document.getElementsByTagName('div'));
+  const divList = Array.from(document.getElementsByClassName('game__card'));
+  const $restart = document.getElementById('restart');
   const numbers = [1, 1, 2, 2, 3, 3, 4, 4];
   const flippedCards = [];
   let selectedIndices = [];
-
-  const bindEventsOnCard = (ele, index, divList) => {
-    ele.addEventListener('click', (e) => {
-      // show the card
-      const currentTarget = e.currentTarget;
-      currentTarget.children[0].style.opacity = 1;
-
-      // push it in to the selected index array
-      selectedIndices.push(index);
-      if (selectedIndices.length === 2) {
-        if(numbers[selectedIndices[0]] === numbers[selectedIndices[1]]) {
-          // if the numbers matches push those indices in flippedCards
-          flippedCards.push(selectedIndices[0], selectedIndices[1]);
-        } else {
-          // if they don`t match, hide those both card and reshuffle.
-          divList[selectedIndices[0]].children[0].style.opacity = 0;
-          divList[selectedIndices[1]].children[0].style.opacity = 0;
-          shuffleArray(numbers, flippedCards);
-          divList.map((div, index) => div.children[0].textContent = numbers[index]);
-        }
-        selectedIndices.length = 0;
-      }
-      // winning condition
-      if (flippedCards.length === numbers.length) {
-        alert('Yay!!');
-        window.location.reload();
-      }
-    }, false);
-  }
 
   const shuffleArray = (arr, excludeIndexList = []) => {
     let currLength = arr.length;
     while (currLength > 0) {
       const randomIndex = Math.floor(Math.random() * currLength--);
       // exclude index list shouldn`t include the random index or the replaced index
-      if(!excludeIndexList.includes(randomIndex) && !excludeIndexList.includes(currLength)) {
-        [arr[randomIndex], arr[currLength]] = [arr[currLength], arr[randomIndex]];
+      if(!excludeIndexList.includes(randomIndex) 
+        && !excludeIndexList.includes(currLength)) {
+        [arr[randomIndex], arr[currLength]] 
+          = [arr[currLength], arr[randomIndex]];
       }
     }
     return arr;
   }
 
-  const initalizeCard = () => {
-    shuffleArray(numbers)
-    divList.map((div, index) => {
-      const span = document.createElement('span');
-      span.textContent = numbers[index];
-      div.appendChild(span);
-      bindEventsOnCard(div, index, divList);
-    });
+  const flipCards = (e, index) => {
+    const currentTarget = e.currentTarget;
+    currentTarget.children[0].classList.add('game__card--rotate');
+
+    // push it in to the selected index array
+    selectedIndices.push(index);
+    if (selectedIndices.length === 2) {
+      if(numbers[selectedIndices[0]] === numbers[selectedIndices[1]]) {
+        // if the numbers matches push those indices in flippedCards
+        flippedCards.push(selectedIndices[0], selectedIndices[1]);
+      } else {
+        const firstOne = selectedIndices[0];
+        const secondOne = selectedIndices[1];
+
+        setTimeout(() => {
+          divList[firstOne].children[0].classList.remove('game__card--rotate');
+          divList[secondOne].children[0].classList.remove('game__card--rotate');
+        }, 500);
+  
+        setTimeout(() => {
+          shuffleArray(numbers, flippedCards);
+          divList.map((div, index) => div.children[0].children[1].textContent = numbers[index]);  
+        }, 600)
+      }
+      selectedIndices.length = 0;
+    }
+    // winning condition
+    if (flippedCards.length === numbers.length) {
+      alert('Yay!!');
+      window.location.reload();
+    }
   }
 
+  const restartGame = () => {
+    divList.forEach(div => div.removeEventListener('click', flipCards, false));
+    initializeGame();
+  }
 
+  const initializeEvents = () => {
+    divList.forEach((div, index) => div.addEventListener('click',
+      e => flipCards(e, index),
+      false
+    ));
+    $restart.addEventListener('click', restartGame, false);
+  }
 
-  document.addEventListener('DOMContentLoaded', initalizeCard, false)
+  const initializeCard = () => {
+    shuffleArray(numbers);
+    divList.map((div, index) => div.children[0].children[1].textContent = numbers[index]);
+  }
+
+  const initializeGame = () => {
+    initializeCard();
+    initializeEvents();
+  }
+
+  document.addEventListener('DOMContentLoaded', initializeGame, false)
 })();
